540af2f16d70d8adef1b756773cb321928f9d323
himarisanto <himariantok@gmail.com>
2025-07-25 01:00:05 +0700

    add: components register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+namespace App\Livewire;
+
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', Auth\Register::class)->name('register');
+
+
+
