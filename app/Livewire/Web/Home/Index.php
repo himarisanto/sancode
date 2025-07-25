38b53753bad6c3109a0631794a9ec7438849cd0f
himarisanto <himarisantok@gmail.com>
2025-07-25 18:06:08 +0700

    card product

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -3,16 +3,51 @@
 namespace App\Livewire\Web\Home;
 
 use App\Models\Slider;
+use App\Models\Product;
 use Livewire\Component;
-use App\Models\Category; // Make sure to import your Category model
+use App\Models\Category;
 
 class Index extends Component
-{
+{    
+    /**
+     * getPopularProducts
+     *
+     * @return void
+     */
+    protected function getPopularProducts()
+    {
+        return Product::with('category', 'ratings.customer')
+            ->withAvg('ratings', 'rating') // Menghitung rata-rata rating
+            ->having('ratings_avg_rating', '>=', 4)
+            ->limit(5) // Batas jumlah produk
+            ->get();
+    }
+    
+    /**
+     * getLatestProducts
+     *
+     * @return void
+     */
+    protected function getLatestProducts()
+    {
+        //get products
+        return Product::query()
+            ->with('category', 'ratings.customer')
+            ->withAvg('ratings', 'rating')
+            ->limit(5)
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.web.home.index', [
-            'categories' => Category::all(), // or whatever query you need
-            'sliders' => Slider::all() // assuming you also need sliders
+
+            'sliders' => Slider::latest()->get(),
+            'categories' => Category::latest()->get(),
+            'popularProducts' => $this->getPopularProducts(),
+            'latestProducts' => $this->getLatestProducts(),
+            
         ]);
     }
 }
