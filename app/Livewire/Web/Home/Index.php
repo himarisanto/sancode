22f0dc10812067b1ffb1827b225fa5b5b15108de
himarisanto <himarisantok@gmail.com>
2025-07-25 15:51:06 +0700

    display slider data

---
+++ app/Livewire/Web/Home/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Web\Home;
+
+use App\Models\Slider;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.web.home.index', [
+            'sliders' => Slider::latest()->get(),
+        ]);
+    }
+}
---
+++ resources/views/livewire/web/home/index.blade.php
@@ -0,0 +1,55 @@
+@section('title')
+Food Store - Eat Your Favorite Foods
+@stop
+
+@section('keywords')
+Food Store, Eat Your Favorite Foods
+@stop
+
+@section('description')
+Food Store - Eat Your Favorite Foods
+@stop
+
+@section('image')
+{{ asset('images/logo.png') }}
+@stop
+
+<div>
+    <div class="container" style="margin-bottom: 120px">
+        
+        <!-- Search Bar -->
+        <x-utils.search-bar />
+
+        <!-- Sliders -->
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-6">
+                <div id="carouselExample" class="carousel slider w-100">
+                    <div class="carousel-inner">
+                        @foreach ($sliders as $key => $slider)
+                            <div class="carousel-item @if($key == 0) active @endif">
+                                <img src="{{ asset('/storage/' . $slider->image) }}" class="d-block w-100 rounded">
+                            </div>
+                        @endforeach
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Categories -->
+
+        <!-- Products Popular -->
+
+        <!-- Products Latest -->
+
+    </div>
+</div>
