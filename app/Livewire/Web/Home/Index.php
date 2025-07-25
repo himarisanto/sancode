2bc5fe9a97f6b3ba09f8171370b66d27752cdab4
himarisanto <himarisantok@gmail.com>
2025-07-25 17:42:00 +0700

    display data categories

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -4,13 +4,15 @@ namespace App\Livewire\Web\Home;
 
 use App\Models\Slider;
 use Livewire\Component;
+use App\Models\Category; // Make sure to import your Category model
 
 class Index extends Component
 {
     public function render()
     {
         return view('livewire.web.home.index', [
-            'sliders' => Slider::latest()->get(),
+            'categories' => Category::all(), // or whatever query you need
+            'sliders' => Slider::all() // assuming you also need sliders
         ]);
     }
 }
