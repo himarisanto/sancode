ae34d7ac973848ab89ff3dce9391954bfd5b1b97
himarisanto <himarisantok@gmail.com>
2025-07-25 11:59:09 +0700

    make: detail orders

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,17 @@
 
 namespace App\Livewire;
 
-use App\Livewire\Auth\Login;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/register', Auth\Register::class)->name('register');
+
 Route::get('/login', Auth\Login::class)->name('login');
 
 Route::middleware('auth:customer')->group(function () {
     
     Route::group(['prefix' => 'account'], function () {
         Route::get('/my-orders', Account\MyOrders\Index::class)->name('account.my-orders.index');
+        Route::get('/my-orders/{snap_token}', Account\MyOrders\Show::class)->name('account.my-orders.show');
     });
+
 });
