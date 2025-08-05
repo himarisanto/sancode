258aef08b28ae6344d958d6f25f5c14e0f23b876
Kezia Regina <keziaregina28@gmail.com>
2025-08-05 09:41:18 +0700

    add: feature product pages

---
+++ app/Livewire/Web/Products/Show.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Livewire\Web\Products;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class Show extends Component
+{    
+    /**
+     * slug
+     *
+     * @var mixed
+     */
+    public $slug;
+    
+    /**
+     * mount
+     *
+     * @param  mixed $slug
+     * @return void
+     */
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    public function render()
+    {
+        $product = Product::query()
+            ->with('category', 'ratings.customer')
+            ->withCount('ratings')
+            ->withAvg('ratings', 'rating')
+            ->where('slug', $this->slug)
+            ->firstOrFail();
+
+        return view('livewire.web.products.show', compact('product'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,35 +4,22 @@ namespace App\Livewire;
 
 use Illuminate\Support\Facades\Route;
 
-//route register
 Route::get('/register', Auth\Register::class)->name('register');
 
-//route login
 Route::get('/login', Auth\Login::class)->name('login');
-
-//route group account
 Route::middleware('auth:customer')->group(function () {
     
     Route::group(['prefix' => 'account'], function () {
         
-        //route my order
         Route::get('/my-orders', Account\MyOrders\Index::class)->name('account.my-orders.index');
-  
-        //route my order show
         Route::get('/my-orders/{snap_token}', Account\MyOrders\Show::class)->name('account.my-orders.show');
-
-        //route my profile
         Route::get('/my-profile', Account\MyProfile\Index::class)->name('account.my-profile');
-
-        //route password
         Route::get('/password', Account\Password\Index::class)->name('account.password');
 
     });
 
 });
-
-//route home
 Route::get('/', Web\Home\Index::class)->name('home');
-
-//route products index
 Route::get('/products', Web\Products\Index::class)->name('web.product.index');
+Route::get('/category/{slug}', Web\Category\Show::class)->name('web.category.show');
+Route::get('/products/{slug}', Web\Products\Show::class)->name('web.product.show');
